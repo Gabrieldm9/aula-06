ba2b5b69d3f60ec1a016d808b2d6f70e0462bae1
gabrieldm9 <gabrieldm9@gmail.com>
2018-08-15 08:18:44 -0300

    mudou mensagem

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -3,7 +3,7 @@ package poo;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Olá mundo");
+        System.out.println("Olá MEU NOME");
     }
 
 
