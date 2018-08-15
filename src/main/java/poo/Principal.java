5101a548f219f926f6efd8e3714b20a030e48032
gabrieldm9 <gabrieldm9@gmail.com>
2018-08-15 08:22:33 -0300

    mudou mensagens

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 
     public static void main(String[] args) {
         System.out.println("Olá MEU NOME");
+        System.out.println("Olá mundo novo");
+        System.out.println("Olá mundo");
     }
 
 
